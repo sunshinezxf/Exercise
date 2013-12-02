589c451402a711ea1d0d59689c4fff2933d536af
sunshine <sunshinezxf@hotmail.com>
2013-12-02 17:29:11 +0800

    header
    Change-Id: Ifa22e276579c6963973b568948ae4629eca4f50b

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sunshine--Home page</title>
+
+<meta charset="utf-8" />
+<meta name="viewport"
+	content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link rel="shortcut icon" href="./material/logo.ico" />
+<link rel="stylesheet" type="text/css" href="./css/core.css" />
+</head>
+<body>
+	<div id="page" class="homepage">
+		<div class="main-header">
+			<div data-id="profile-bar-list" class="header-profile-bar">
+				<div class="row grid-layout">
+					<a class="logo" href="/"><img alt="logo" src="./material/logo.jpg" /></a>
+					<div class="search">
+						<nav>
+							<input class="search_input" autocomplete="off"> <a>Search</a>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
